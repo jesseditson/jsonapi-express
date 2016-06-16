2e81bee3731f8376dc99e1f16f8acd9c6b94b9b9
Jesse Ditson <jesse.ditson@gmail.com>
2016-06-15 21:47:27 -0700

    add ember addon for parsing schemas in to ember models

---
+++ addon.js
@@ -0,0 +1,28 @@
+// var Funnel = require('broccoli-funnel');
+// var JSONModule = require('broccoli-json-module');
+// var MergeTrees = require('broccoli-merge-trees');
+// var WatchedDir = require('broccoli-source').WatchedDir;
+
+module.exports = {
+  name: 'jsonapi-express',
+
+  isDevelopingAddon: function() { return true; },
+
+  // TODO: figure out how to add the schemas to the resolver via `import schemas`
+  // included: function included(app, parentAddon) {
+  //   this._super.included(app);
+  //
+  //
+  //   // var target = (parentAddon || app);
+  //   //
+  //   // target.import(this.schemas);
+  // },
+  // treeForAddon(tree) {
+  //   tree = this._super.treeForAddon.call(this, tree);
+  //
+  // },
+  // treeForApp(tree) {
+  //   var schemasFolder = new WatchedDir('schemas');
+  //   return mergeTrees([tree, jsonModule(schemasFolder)]);
+  // }
+}
